68e77d4bcfc705300820114a40ac6c8fc8b0ef73
Nikita <nikita.tolstv@gmail.com>
2025-04-27 15:34:00 +0300

    start

---
+++ src/main/java/github/nikandpro/BankAppApplication.java
@@ -0,0 +1,13 @@
+package github.nikandpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class BankAppApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
